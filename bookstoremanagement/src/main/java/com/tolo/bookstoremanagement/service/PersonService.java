94e9b2d990fd0d6483d0caa90d178c7e6bacb4f6
Marken <nctuan.ityu@gmail.com>
2021-05-29 13:30:32 +0100

    Initial commit

---
+++ bookstoremanagement/src/main/java/com/tolo/bookstoremanagement/service/PersonService.java
@@ -0,0 +1,57 @@
+package com.tolo.bookstoremanagement.service;
+
+import com.tolo.bookstoremanagement.dao.PersonDAO;
+import com.tolo.bookstoremanagement.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+    
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonService(@Qualifier("personData") PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    public int addPerson(Person person) {
+        return personDAO.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople() {
+        return personDAO.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDAO.selectPersonById(id);
+    }
+
+    public  int deletePerson(UUID id) {
+        return personDAO.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person newPerson) {
+        return personDAO.updatePersonById(id, newPerson);
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
