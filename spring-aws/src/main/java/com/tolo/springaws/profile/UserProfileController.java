147c96165c953a10b3c95b24cf7e5bd400d07a9e
Marken <nctuan.ityu@gmail.com>
2021-11-17 21:17:21 +0000

    reactjs as frontend

--- spring-aws/src/main/java/com/tolo/springaws/profile/UserProfileController.java
+++ spring-aws/src/main/java/com/tolo/springaws/profile/UserProfileController.java
@@ -1,12 +1,13 @@
 package com.tolo.springaws.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/user-profile")
@@ -22,5 +23,17 @@ public class UserProfileController {
     @GetMapping
     public List<UserProfile> getUserProfiles() {
         return userProfileService.getUserProfiles();
+    }
+
+    @PostMapping(
+            path = "{userProfileId}/image/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void uploadImage(@PathVariable("userProfileId") UUID userProfileId,
+                            @RequestParam("file")MultipartFile file) {
+
+        userProfileService.uploadImage(userProfileId, file);
+
     }
 }
--- spring-aws/src/main/java/com/tolo/springaws/profile/UserProfileService.java
+++ spring-aws/src/main/java/com/tolo/springaws/profile/UserProfileService.java
@@ -2,8 +2,10 @@ package com.tolo.springaws.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -17,5 +19,9 @@ public class UserProfileService {
 
     List<UserProfile> getUserProfiles() {
         return userProfileRepository.getUserProfiles();
+    }
+
+    public void uploadImage(UUID userProfileId, MultipartFile file) {
+
     }
 }
